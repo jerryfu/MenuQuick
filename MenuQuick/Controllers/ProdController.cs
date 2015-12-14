d8c438a76ee6119a8629a2b6b852cdea4c66558a
Jerry Fu <fusun@ms7.hinet.net>
2015-12-14 11:07:57 +0800

    Start Project

---
+++ MenuQuick/Controllers/ProdController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MenuQuick.DB;
+namespace MenuQuick.Controllers
+{
+
+    public class ProdController : ApiController
+    {
+        public IEnumerable<Prod> Get()
+        {
+
+            var db = new MenuQuickEntities();
+            var item = db.Prod.OrderBy(x => x.item).AsEnumerable();
+
+            return item;
+        }
+
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        public void Post([FromBody]string value)
+        {
+        }
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        public void Delete(int id)
+        {
+        }
+    }
+}
