3709ba6b876b96e165d411f1713141abcd2a83c5
Jerry Fu <fusun@ms7.hinet.net>
2015-12-28 21:23:52 +0800

    add json data

--- MenuQuick/Controllers/ProdController.cs
+++ MenuQuick/Controllers/ProdController.cs
@@ -10,14 +10,28 @@ namespace MenuQuick.Controllers
 
     public class ProdController : ApiController
     {
-        public IEnumerable<Prod> Get()
+        public IEnumerable<m_Prod> Get()
         {
 
             var db = new MenuQuickEntities();
-            var item = db.Prod.OrderBy(x => x.item).AsEnumerable();
+            var item = db.Prod.OrderBy(x => x.item).Select(x=> new m_Prod(){
+                prod_id = x.prod_id,
+                prod_name = x.prod_name,
+                price =x.price,
+                imgsrc = "http://menuquick.fly.idv.tw/p" + x.prod_id + ".jpg"}).AsEnumerable();
 
             return item;
         }
+
+        public class m_Prod {
+            public int prod_id { get; set; }
+            public string cust_email { get; set; }
+            public int item { get; set; }
+            public string prod_name { get; set; }
+            public int price { get; set; }
+            public string imgsrc { get; set; }
+        }
+
 
         public string Get(int id)
         {
